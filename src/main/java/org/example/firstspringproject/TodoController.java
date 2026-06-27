672c45eb1c876e187555f932acfe6934b6624322
chiragsharma7495 <chiragsharma7495@gmail.com>
2026-06-27 18:27:01 +0530

    Added Delete Mapping annotation

--- src/main/java/org/example/firstspringproject/TodoController.java
+++ src/main/java/org/example/firstspringproject/TodoController.java
@@ -1,14 +1,13 @@
 package org.example.firstspringproject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
+@RequestMapping("/api/v1/todos")
 public class TodoController {
 
     private static List<Todo> TodoList;
@@ -19,15 +18,34 @@ public class TodoController {
         TodoList.add(new Todo(2, true , "Todo 2" , 2));
     }
 
-    @GetMapping("/todos")
+    @GetMapping
     public List <Todo> getTodos(){
         return TodoList;
     }
 
-    @PostMapping("/todos")
+    @PostMapping
     public Todo createTodo(@RequestBody Todo newTodo){
         TodoList.add(newTodo);
         return newTodo;
+    }
+
+    @GetMapping("/{todoID}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable("todoID") long todoID){
+        for(Todo todo : TodoList){
+            if(todo.getId() == todoID) return ResponseEntity.ok(todo);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{todoID}")
+    public ResponseEntity<String> deleteTodo(@PathVariable int todoID){
+        for(Todo todo : TodoList){
+            if(todo.getId() == todoID) {
+                TodoList.remove(todo);
+                return ResponseEntity.ok("Todo Deleted Successfully");
+            }
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
